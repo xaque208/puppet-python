d6be29c5ef4a089ffda8610bfddb1f69b73fc646
Zach Leslie <xaque208@gmail.com>
2019-08-13 20:29:15 -0700

    Progress

--- spec/unit/facter/util/fact_pip_version_spec.rb
+++ spec/unit/facter/util/fact_pip_version_spec.rb
@@ -5,6 +5,8 @@ describe Facter::Util::Fact do
     Facter.clear
   end
 
+  let(:exec) {}
+
   let(:pip_version_output) do
     <<-EOS
 pip 6.0.6 from /opt/boxen/homebrew/Cellar/python/2.7.9/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/pip-6.0.6-py2.7.egg (python 2.7)
@@ -14,17 +16,15 @@ EOS
   describe 'pip_version' do
     context 'returns pip version when pip present' do
       it do
-        Facter::Util::Resolution.stubs(:exec)
-        Facter::Util::Resolution.expects(:which).with('pip').returns(true)
-        Facter::Util::Resolution.expects(:exec).with('pip --version 2>&1').returns(pip_version_output)
+        expect(Facter::Util::Resolution).to receive(:which).with('pip').and_return(true)
+        expect(Facter::Util::Resolution).to receive(:exec).with('pip --version 2>&1').and_return(pip_version_output)
         Facter.value(:pip_version).should == '6.0.6'
       end
     end
 
     context 'returns nil when pip not present' do
       it do
-        Facter::Util::Resolution.stubs(:exec)
-        Facter::Util::Resolution.expects(:which).with('pip').returns(false)
+        expect(Facter::Util::Resolution).to receive(:which).with('pip').and_return(false)
         Facter.value(:pip_version).should.nil?
       end
     end
--- spec/unit/facter/util/fact_virtualenv_version_spec.rb
+++ spec/unit/facter/util/fact_virtualenv_version_spec.rb
@@ -5,6 +5,8 @@ describe Facter::Util::Fact do
     Facter.clear
   end
 
+  let(:exec) {}
+
   let(:virtualenv_version_output) do
     <<-EOS
 12.0.7
@@ -14,17 +16,15 @@ EOS
   describe 'virtualenv_version' do
     context 'returns virtualenv version when virtualenv present' do
       it do
-        Facter::Util::Resolution.stubs(:exec)
-        Facter::Util::Resolution.expects(:which).with('virtualenv').returns(true)
-        Facter::Util::Resolution.expects(:exec).with('virtualenv --version 2>&1').returns(virtualenv_version_output)
+        expect(Facter::Util::Resolution).to receive(:which).with('virtualenv').and_return(true)
+        expect(Facter::Util::Resolution).to receive(:exec).with('virtualenv --version 2>&1').and_return(virtualenv_version_output)
         Facter.value(:virtualenv_version).should == '12.0.7'
       end
     end
 
     context 'returns nil when virtualenv not present' do
       it do
-        Facter::Util::Resolution.stubs(:exec)
-        Facter::Util::Resolution.expects(:which).with('virtualenv').returns(false)
+        expect(Facter::Util::Resolution).to receive(:which).with('virtualenv').and_return(false)
         Facter.value(:virtualenv_version).should.nil?
       end
     end
