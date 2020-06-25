d78676b8e731de724dad3aff155ce882d4ef51c0
Tim Meusel <tim@bastelfreak.de>
2020-06-25 10:58:53 +0200

    fix virtualenv_version fact for version 20
    latest virtualenv is 20.0.23. This version leads to the following error:
    ```
    Error: Facter: error while resolving custom fact "virtualenv_version": undefined method `[]' for nil:NilClass
    ```
    
    I reproduced it in irb:
    
    ```
    irb(main):001:0> `virtualenv --version 2>&1`.match(%r{^(\d+\.\d+\.?\d*).*$})
    => nil
    irb(main):002:0> `virtualenv --version 2>&1`
    => "virtualenv 20.0.23 from /usr/lib/python3.8/site-packages/virtualenv/__init__.py\n"
    irb(main):003:0> `virtualenv --version 2>&1`.match(%r{(\d+\.\d+\.?\d*).*$})
    => #<MatchData "20.0.23 from /usr/lib/python3.8/site-packages/virtualenv/__init__.py" 1:"20.0.23">
    irb(main):004:0> `virtualenv --version 2>&1`.match(%r{(\d+\.\d+\.?\d*).*$})[1]
    => "20.0.23"
    irb(main):005:0>
    ```
    
    This code not only fixes it, it's a direct copy from voxpupuli/python.
    merging this brings us also closer to solving https://github.com/xaque208/puppet-python/issues/49

--- spec/unit/facter/util/fact_virtualenv_version_spec.rb
+++ spec/unit/facter/util/fact_virtualenv_version_spec.rb
@@ -5,27 +5,52 @@ describe Facter::Util::Fact do
     Facter.clear
   end
 
-  let(:exec) {}
-
-  let(:virtualenv_version_output) do
+  let(:virtualenv_old_version_output) do
     <<-EOS
 12.0.7
 EOS
   end
 
-  describe 'virtualenv_version' do
+  let(:virtualenv_new_version_output) do
+    <<-EOS
+virtualenv 20.0.17 from /opt/python/lib/python3.5/site-packages/virtualenv/__init__.py
+EOS
+  end
+
+  describe 'virtualenv_version old' do
     context 'returns virtualenv version when virtualenv present' do
       it do
-        allow(Facter::Util::Resolution).to receive(:which).with('virtualenv') { true }
-        allow(Facter::Util::Resolution).to receive(:exec).with('virtualenv --version 2>&1') { virtualenv_version_output }
-        Facter.value(:virtualenv_version).should == '12.0.7'
+        Facter::Util::Resolution.stubs(:exec)
+        Facter::Util::Resolution.expects(:which).with('virtualenv').returns(true)
+        Facter::Util::Resolution.expects(:exec).with('virtualenv --version 2>&1').returns(virtualenv_old_version_output)
+        expect(Facter.value(:virtualenv_version)).to eq('12.0.7')
       end
     end
 
     context 'returns nil when virtualenv not present' do
       it do
-        expect(Facter::Util::Resolution).to receive(:which).with('virtualenv').and_return(false)
-        Facter.value(:virtualenv_version).should.nil?
+        Facter::Util::Resolution.stubs(:exec)
+        Facter::Util::Resolution.expects(:which).with('virtualenv').returns(false)
+        expect(Facter.value(:virtualenv_version)).to eq(nil)
+      end
+    end
+  end
+
+  describe 'virtualenv_version new' do
+    context 'returns virtualenv version when virtualenv present' do
+      it do
+        Facter::Util::Resolution.stubs(:exec)
+        Facter::Util::Resolution.expects(:which).with('virtualenv').returns(true)
+        Facter::Util::Resolution.expects(:exec).with('virtualenv --version 2>&1').returns(virtualenv_new_version_output)
+        expect(Facter.value(:virtualenv_version)).to eq('20.0.17')
+      end
+    end
+
+    context 'returns nil when virtualenv not present' do
+      it do
+        Facter::Util::Resolution.stubs(:exec)
+        Facter::Util::Resolution.expects(:which).with('virtualenv').returns(false)
+        expect(Facter.value(:virtualenv_version)).to eq(nil)
       end
     end
   end
